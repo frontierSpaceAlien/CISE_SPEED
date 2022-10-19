53501fd99f4adeb7e33dd551078539363e9c1556
frontierSpaceAlien <yxx6616@autuni.ac.nz>
2022-10-19 16:15:00 +1300

    updated files for heroku

---
+++ frontend/src/pages/Analyse-Article.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AnalyseArticle = () =>  {
+     return (
+      <div>
+        <h2>Analyse Articles</h2>
+            <p>This page will allow analysts to analyse articles</p>
+        </div>
+    );
+}
+ 
+export default AnalyseArticle;
---
+++ frontend/src/pages/Moderate-Article.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ModerateArticles = () =>  {
+     return (
+      <div>
+        <h2>Moderate Articles</h2>
+            <p>This page will allow moderators to moderate articles</p>
+        </div>
+    );
+}
+ 
+export default ModerateArticles;
